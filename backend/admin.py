706939752067716663cf9cf7c9fbf09d0aa5f683
KonstantinDichev <kdichev.nl@gmail.com>
2023-06-11 19:27:51 +0200

    Item model and serializer

--- backend/admin.py
+++ backend/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Contact
-
+from models.Contact.contact_model import Contact
+from models.Item.item_model import Order, Item
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'description', 'email')
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'item')
