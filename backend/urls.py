4fd7f1e9575462a34c393c71f9e72a9e24c1cac7
KonstantinDichev <kdichev.nl@gmail.com>
2023-06-12 18:56:35 +0200

    url updates

--- backend/urls.py
+++ backend/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
 from django.contrib import admin
-from core import views as core_views
+from views.viewsets.item_viewset import ItemViewSet, OrderViewSet
+from views.contact_view import ContactAPIView
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 router = routers.DefaultRouter()
+router.register(r'item', ItemViewSet, basename='item')
+router.register(r'order', OrderViewSet, basename='order')
 
 urlpatterns = router.urls
 
 urlpatterns += [
     path('admin/', admin.site.urls),
-    path('contact/', core_views.ContactAPIView.as_view()),
+    path('contact/', ContactAPIView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
